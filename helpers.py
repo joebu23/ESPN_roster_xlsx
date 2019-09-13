fee127aae104d7983822f296be5c7935011098fb
Joe Royston <joebu23@users.noreply.github.com>
2019-09-13 19:32:42 -0400

    initial upload

---
+++ helpers.py
@@ -0,0 +1,179 @@
+def GetField(array, index):
+    return array[index].strip()
+
+
+def GetAbvPositionSoccer(position):
+    if position.find('Goalkeeper') != -1:
+        return 'GK'
+    if position.find('Defender') != -1:
+        return 'D'
+    if position.find('Midfielder') != -1:
+        return 'M'
+    if position.find('Forward') != -1:
+        return 'F'
+    else:
+        return position
+
+
+def GetPositionSoccer(position):
+    if position == 'G':
+        return 'Goalkeeper'
+    if position == 'D':
+        return 'Defender'
+    if position == 'M':
+        return 'Midfielder'
+    if position == 'F':
+        return 'Forward'
+    if position == 'M/F':
+        return 'Mid/Forward'
+    else:
+        return position
+
+
+def GetPositionVolleyball(position):
+    if position == 'L/DS':
+        return 'Defensive Specialist'
+    if position == 'DS':
+        return 'Defensive Specialist'
+    if position == 'L':
+        return 'Libero'
+    if position == 'MB':
+        return 'Middle Blocker'
+    if position == 'S':
+        return 'Setter'
+    if position == 'OH':
+        return 'Outside Hitter'
+    else:
+        return position
+
+
+def FixState(hometown):
+    if hometown.find('Mich.') != -1:
+        return hometown.replace('Mich.', 'MI')
+    if hometown.find('Ohio') != -1:
+        return hometown.replace('Ohio', 'OH')
+    if hometown.find('Ill.') != -1:
+        return hometown.replace('Ill.', 'IL')
+    if hometown.find('Ind.') != -1:
+        return hometown.replace('Ind.', 'IN')
+    if hometown.find('Fla.') != -1:
+        return hometown.replace('Fla.', 'FL')
+    if hometown.find('Ga.') != -1:
+        return hometown.replace('Ga.', 'GA')
+    if hometown.find('Ky.') != -1:
+        return hometown.replace('Ky.', 'KY')
+        if hometown.find('Calif.') != -1:
+            return hometown.replace('Calif.', 'CA')
+        if hometown.find('Wisc.') != -1:
+            return hometown.replace('Wisc.', 'WI')
+    if hometown.find('Wis.') != -1:
+        return hometown.replace('Wis.', 'WI')
+        if hometown.find('Pa.') != -1:
+            return hometown.replace('Pa.', 'PA')
+    if hometown.find('Kan.') != -1:
+        return hometown.replace('Kan.', 'KS')
+    if hometown.find('N.Y.') != -1:
+        return hometown.replace('N.Y.', 'NY')
+    if hometown.find('N.J.') != -1:
+        return hometown.replace('N.J.', 'NJ')
+    if hometown.find('Minn.') != -1:
+        return hometown.replace('Minn.', 'MN')
+    if hometown.find('Ala.') != -1:
+        return hometown.replace('Ala.', 'AL')
+    if hometown.find('Texas') != -1:
+        return hometown.replace('Texas', 'TX')
+    if hometown.find('Mo.') != -1:
+        return hometown.replace('Mo.', 'MO')
+    if hometown.find('N.C.') != -1:
+        return hometown.replace('N.C.', 'NC')
+    if hometown.find('Ariz.') != -1:
+        return hometown.replace('Ariz.', 'AZ')
+    if hometown.find('Neb.') != -1:
+        return hometown.replace('Neb.', 'NE')
+    if hometown.find('Iowa') != -1:
+        return hometown.replace('Iowa', 'IA')
+
+    return hometown
+
+
+def GetClass(playerclass, abbr):
+    if playerclass.find('fr') != -1:
+        if abbr:
+            return "FR"
+        else:
+            return "Freshman"
+    elif playerclass.find('so') != -1:
+        if abbr:
+            return "SO"
+        else:
+            return "Sophomore"
+    elif playerclass.find('jr') != -1:
+        if abbr:
+            return "JR"
+        else:
+            return "Junior"
+    elif playerclass.find('sr') != -1:
+        if abbr:
+            return "SR"
+        else:
+            return "Senior"
+    else:
+        return playerclass.capitalize()
+
+
+def GetNumberedClass(playerclass):
+    playerclass = playerclass.lower()
+    if playerclass == 'fr':
+        return "1"
+    elif playerclass == 'fr.':
+        return "1"
+    elif playerclass == 'so':
+        return "2"
+    elif playerclass == 'r-so':
+        return "2"
+    elif playerclass == 'so.':
+        return "2"
+    elif playerclass == 'jr':
+        return "3"
+    elif playerclass == 'jr.':
+        return "3"
+    elif playerclass == 'r-jr':
+        return "3"
+    elif playerclass == 'sr':
+        return "4"
+    elif playerclass == 'sr.':
+        return "4"
+    elif playerclass == 'r-sr':
+        return "4"
+    elif playerclass == 'gr':
+        return "4"
+    elif playerclass == 'r-fr':
+        return "1"
+    elif playerclass == 'rfr':
+        return "1"
+    else:
+        return "0"
+
+
+def GetFormattedHeight(heightString):
+    height = heightString.replace('-', "'", 1)
+    return height + '"'
+
+
+def GetFormattedHometown(hometownString):
+    if hometownString.find('/') != -1:
+        hometownString = hometownString.split("/")[0].strip()
+
+    hometownString = hometownString.replace('-', ',')
+
+    return FixState(hometownString)
+
+
+def GetThrowingArm(batsThrows):
+    throws = batsThrows.split('/')[1]
+    return throws
+
+
+def GetBatting(batsThrows):
+    bats = batsThrows.split('/')[0]
+    return bats
---
+++ players_CSV.py
@@ -0,0 +1,148 @@
+import csv
+import xlwt
+import sys
+import helpers
+import numbers
+
+myTabfile = open("players.txt", "r")
+myReadtab = csv.reader(myTabfile, delimiter='\t')
+
+
+class Player:
+
+    def __init__(self, number, firstname, lastname, position, abvPosition, fullClass, abbrClass, numClass, height, weight, hometown, batting, throwing):
+        self.number = number
+        self.firstname = firstname
+        self.lastname = lastname
+        self.position = position
+        self.abvPosition = abvPosition
+        self.fullClass = fullClass
+        self.abbrClass = abbrClass
+        self.numClass = numClass
+        self.height = height
+        self.weight = weight
+        self.hometown = hometown
+        self.batting = batting
+        self.throwing = throwing
+
+
+# set these according to what field the item is in.
+playernumber = 0
+playername = 2
+playerposition = 3
+pclass = 4
+playerheight = 5
+playerweight = 4
+playerhometown = 6
+batsThrows = 4
+filename = sys.argv[1] + ".csv"
+reverseName = sys.argv[2]
+sport = sys.argv[3]
+
+players = []
+for x in range(0, 100):
+    players.insert(x, Player("", "", "", "", "",
+                             "", "", "", "", "", "", "", ""))
+
+for row in myReadtab:
+    PlayerInfo = '|'.join(row)
+    SplitInfo = PlayerInfo.split('|')
+    print(SplitInfo)
+
+    # get number
+    newPlayerNumber = helpers.GetField(SplitInfo, playernumber)
+
+    # figure out name
+    newPlayerName = ""
+    if reverseName == True:
+        nameRaw = helpers.GetField(SplitInfo, playername)
+        newPlayerName = nameRaw.split(',')[1] + " " + nameRaw.split(',')[0]
+    else:
+        newPlayerName = helpers.GetField(SplitInfo, playername)
+
+    newPlayerFirstname = newPlayerName.split(' ')[0]
+    newPlayerLastname = newPlayerName.split(' ')[1]
+
+    # position
+    newPlayerPosition = helpers.GetField(SplitInfo, playerposition)
+
+    if sport == 'Soccer':
+        newPlayerPositionAbv = helpers.GetAbvPositionSoccer(newPlayerPosition)
+        newPlayerPosition = helpers.GetPositionSoccer(newPlayerPosition)
+    if sport == 'Volleyball':
+        newPlayerPositionAbv = newPlayerPosition
+        newPlayerPosition = helpers.GetPositionVolleyball(newPlayerPosition)
+    else:
+        newPlayerPositionAbv = newPlayerPosition
+
+    # class
+    playerclass = helpers.GetField(SplitInfo, pclass).replace('.', '').lower()
+    newPlayerClassAbbr = helpers.GetClass(playerclass, True)
+    newPlayerClassFull = helpers.GetClass(playerclass, False)
+    newPlayerClassNum = helpers.GetNumberedClass(playerclass)
+
+    # height
+    newPlayerHeight = helpers.GetFormattedHeight(
+        helpers.GetField(SplitInfo, playerheight))
+
+    # weight
+    newPlayerWeight = helpers.GetField(SplitInfo, playerweight)
+
+    # hometown
+    newPlayerHometown = helpers.GetFormattedHometown(
+        helpers.GetField(SplitInfo, playerhometown))
+
+    # baseball stuffs
+    if sport == "Baseball":
+        newPlayerBatting = helpers.GetBatting(
+            helpers.GetField(SplitInfo, batsThrows))
+        newPlayerThrowing = helpers.GetThrowingArm(
+            helpers.GetField(SplitInfo, batsThrows))
+    else:
+        newPlayerBatting = " "
+        newPlayerThrowing = " "
+
+    errors = "Problem processing: "
+    try:
+        playerNumberIndex = int(SplitInfo[playernumber].strip())
+        players.insert(playerNumberIndex, Player(newPlayerNumber, newPlayerFirstname, newPlayerLastname, newPlayerPosition, newPlayerPositionAbv,
+                                                 newPlayerClassFull, newPlayerClassAbbr, newPlayerClassNum, newPlayerHeight, newPlayerWeight, newPlayerHometown, newPlayerBatting, newPlayerThrowing))
+
+    except:
+        errors = errors + "There was a problem with a player\n"
+
+
+# open the file and write to it
+f = open(filename, 'w')
+
+for x in range(0, 100):
+    # print players[x].lastname
+    if sport == 'Soccer':
+        playerString = str(x) + "," + str(players[x].number) + "," + players[x].firstname + "," + players[x].lastname + "," + players[x].abvPosition + \
+            ',' + players[x].position + "," + players[x].height + ", ," + \
+            players[x].abbrClass + ',"' + players[x].hometown + '"\n'
+    elif sport == 'Volleyball':
+        playerString = str(x) + "," + str(players[x].number) + "," + players[x].firstname + "," + players[x].lastname + "," + players[x].abvPosition + \
+            ',' + players[x].position + "," + players[x].height + ", ," + \
+            players[x].abbrClass + ',"' + players[x].hometown + '"\n'
+    elif sport == 'Basketball':
+        playerString = str(x) + "," + str(players[x].number) + "," + players[x].firstname + "," + players[x].lastname + "," + \
+            players[x].position + "," + players[x].height + "," + \
+            players[x].abbrClass + ',"' + players[x].hometown + '"\n'
+    elif sport == 'Lacrosse':
+        playerString = str(x) + "," + str(players[x].number) + "," + players[x].firstname + "," + players[x].lastname + ", ," + \
+            players[x].position + ", ," + players[x].height + ", ," + \
+            players[x].abbrClass + ',"' + players[x].hometown + '"\n'
+    elif sport == 'Baseball':
+        playerString = str(x) + "," + players[x].lastname + "," + players[x].firstname + "," + players[x].number + ", ," + players[x].position + "," + players[x].batting + \
+            "," + players[x].throwing + ',' + players[x].hometown + ',' + \
+            players[x].height + "," + players[x].weight + \
+            "," + players[x].fullClass + '\n'
+    else:
+        playerString = str(x) + "," + str(players[x].number) + "," + players[x].firstname + "," + players[x].lastname + "," + players[x].position + \
+            "," + players[x].height + "," + players[x].weight + ',"' + \
+            players[x].hometown + '",' + players[x].numClass + "\n"
+    f.write(playerString)
+
+f.write(errors)
+f.close()
